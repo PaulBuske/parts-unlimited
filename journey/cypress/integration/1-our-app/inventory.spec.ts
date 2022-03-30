b2bbe7b93b70f119bf4cc03cb84d919145f79171
Paul Buske <paul.a.buske.mil@army.mil>
2022-03-30 15:13:46 -0500

    added failing cypress test to add to product quantity.

--- journey/cypress/integration/1-our-app/inventory.spec.ts
+++ journey/cypress/integration/1-our-app/inventory.spec.ts
@@ -4,7 +4,36 @@ const addProduct = (product: string) => {
   cy.findByLabelText("Product to add").type(product);
   cy.findByRole("button").click();
 }
+
+function viewInventory() {
+  cy.visit("http://localhost:8080");
+}
+
+function createAProduct() {
+  addProduct("product1");
+}
+
+function increaseProductByOneHundred() {
+  cy.findByLabelText("quantity").type("100");
+  cy.findByRole("button", {name: /update/i}).click();
+}
+
+function expectToSeeOneHundred() {
+  cy.findByText("100")
+}
+
 describe("inventory", () => {
+
+  describe("when I add additional inventory to existing inventory", () =>{
+    it('should display the increased amount for the product', () => {
+      viewInventory();
+      createAProduct();
+      increaseProductByOneHundred();
+      expectToSeeOneHundred();
+        }
+    );
+  })
+
   describe("when adding a product offering", () => {
     it("should display the new product with a default quantity of 0", () => {
       cy.visit("http://localhost:8080");
